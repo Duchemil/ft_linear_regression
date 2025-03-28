a94c63f4836e8c0464861bf4bac71e45bd64e1a8
Duchemil <Lucas-76700@hotmail.fr>
2025-03-28 15:35:58 +0100

    Theta0 and Theta1 found

---
+++ train.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+try:
+    data = pd.read_csv('data.csv')
+    x = data['km'].tolist()
+    y = data['price'].tolist()
+except Exception as e:
+    print("\033[H\033[J", end="")
+    print("An error occurred while processing the data.")
+
+
+n = len(x)
+if n > 0:
+    mean_x = sum(x) / n
+    mean_y = sum(y) / n
+
+    # Calculate the slope (thet1)
+    numerator = sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(n))
+    denominator = sum((x[i] - mean_x) ** 2 for i in range(n))
+    thet1 = numerator / denominator
+
+    # Calculate the intercept (theta0)
+    theta0 = mean_y - thet1 * mean_x
+
+    # Clear the console
+    print("\033[H\033[J", end="")
+    print(f"Theta0 (Intercept): {theta0}, Theta1 (Slope): {thet1}")
+
+    plt.scatter(x, y)
+    regression_line = [theta0 + thet1 * i for i in x]
+    plt.plot(x, regression_line, color='red', label='Regression Line')
+    plt.xlabel('Km')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.title('Linear Regression')
+    plt.show()
+else:
+    # Clear the console and print no data points
+    print("\033[H\033[J", end="")
+    print("No data points available for regression.")
